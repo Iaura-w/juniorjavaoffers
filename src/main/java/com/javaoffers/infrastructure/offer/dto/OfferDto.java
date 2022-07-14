0d6d3e7d87f37f46ba79f85e0cd182a5a6a3c23c
lauur <92169813+lauur@users.noreply.github.com>
2022-07-14 16:25:55 +0200

    added lombok annotations

--- src/main/java/com/javaoffers/infrastructure/offer/dto/OfferDto.java
+++ src/main/java/com/javaoffers/infrastructure/offer/dto/OfferDto.java
@@ -1,8 +1,21 @@
 package com.javaoffers.infrastructure.offer.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OfferDto {
     private String title;
     private String company;
     private String salary;
-    private String offerLink;
+    private String offerUrl;
 }
