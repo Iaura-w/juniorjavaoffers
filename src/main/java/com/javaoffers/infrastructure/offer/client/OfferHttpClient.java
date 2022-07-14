6c304fb0b4d541f1156e1fe222d234d9853fd7d5
lauur <92169813+lauur@users.noreply.github.com>
2022-07-14 16:10:45 +0200

    added offer classes

---
+++ src/main/java/com/javaoffers/infrastructure/offer/client/OfferHttpClient.java
@@ -0,0 +1,14 @@
+package com.javaoffers.infrastructure.offer.client;
+
+import com.javaoffers.infrastructure.RemoteOfferClient;
+import com.javaoffers.infrastructure.offer.dto.OfferDto;
+
+import java.util.List;
+
+public class OfferHttpClient implements RemoteOfferClient {
+
+    @Override
+    public List<OfferDto> getOffers() {
+        return null;
+    }
+}
---
+++ src/main/java/com/javaoffers/infrastructure/offer/dto/OfferDto.java
@@ -0,0 +1,8 @@
+package com.javaoffers.infrastructure.offer.dto;
+
+public class OfferDto {
+    private String title;
+    private String company;
+    private String salary;
+    private String offerLink;
+}
