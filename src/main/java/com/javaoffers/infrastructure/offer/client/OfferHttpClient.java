71014d5f2e1b6fecacdb96930f9c41062e2eeaf1
lauur <92169813+lauur@users.noreply.github.com>
2022-07-14 16:46:19 +0200

    implemented getOffers

--- src/main/java/com/javaoffers/infrastructure/offer/client/OfferHttpClient.java
+++ src/main/java/com/javaoffers/infrastructure/offer/client/OfferHttpClient.java
@@ -2,13 +2,43 @@ package com.javaoffers.infrastructure.offer.client;
 
 import com.javaoffers.infrastructure.RemoteOfferClient;
 import com.javaoffers.infrastructure.offer.dto.OfferDto;
+import lombok.AllArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 
+@AllArgsConstructor
 public class OfferHttpClient implements RemoteOfferClient {
+
+    private final RestTemplate restTemplate;
+    private final String uri;
 
     @Override
     public List<OfferDto> getOffers() {
-        return null;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        final HttpEntity<HttpHeaders> requestEntity = new HttpEntity<>(httpHeaders);
+
+        try {
+            ResponseEntity<List<OfferDto>> response = restTemplate.exchange(
+                    uri,
+                    HttpMethod.GET,
+                    requestEntity,
+                    new ParameterizedTypeReference<List<OfferDto>>() {});
+
+            List<OfferDto> offerList = response.getBody();
+
+            return (offerList!=null) ? offerList : Collections.emptyList();
+        } catch (RestClientException e) {
+            return Collections.emptyList();
+        }
     }
 }
