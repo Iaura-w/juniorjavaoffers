e6127b0e2a189b7a6d2fd1aa3d7709379f1f64a1
lauur <92169813+lauur@users.noreply.github.com>
2022-07-15 10:21:05 +0200

    updated unit tests

--- src/test/java/com/javaoffers/infrastructure/offer/client/OfferHttpClientTest.java
+++ src/test/java/com/javaoffers/infrastructure/offer/client/OfferHttpClientTest.java
@@ -2,23 +2,16 @@ package com.javaoffers.infrastructure.offer.client;
 
 import com.javaoffers.infrastructure.offer.dto.OfferDto;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class OfferHttpClientTest {
+class OfferHttpClientTest implements SampleExchangeResponse, SampleOfferResponse{
 
 
     @Test
@@ -27,12 +20,8 @@ class OfferHttpClientTest {
         RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
         String uri = "test";
 
-        when(restTemplate.exchange(
-                ArgumentMatchers.anyString(),
-                ArgumentMatchers.any(HttpMethod.class),
-                ArgumentMatchers.any(),
-                ArgumentMatchers.<ParameterizedTypeReference<List<OfferDto>>>any()
-        )).thenReturn(new ResponseEntity<>(Collections.singletonList(new OfferDto()), HttpStatus.ACCEPTED));
+        when(getExchange(restTemplate))
+                .thenReturn(responseWithOneOffer());
 
         OfferHttpClient offerHttpClient = new OfferHttpClient(restTemplate, uri);
 
@@ -49,12 +38,8 @@ class OfferHttpClientTest {
         RestTemplate restTemplate = mock(RestTemplate.class);
         String uri = "test";
 
-        when(restTemplate.exchange(
-                ArgumentMatchers.anyString(),
-                ArgumentMatchers.any(HttpMethod.class),
-                ArgumentMatchers.any(),
-                ArgumentMatchers.<ParameterizedTypeReference<List<OfferDto>>>any()
-        )).thenReturn(new ResponseEntity<>(Collections.emptyList(), HttpStatus.ACCEPTED));
+        when(getExchange(restTemplate))
+                .thenReturn(responseWithNoOffers());
 
         OfferHttpClient offerHttpClient = new OfferHttpClient(restTemplate, uri);
 
@@ -71,15 +56,8 @@ class OfferHttpClientTest {
         RestTemplate restTemplate = mock(RestTemplate.class);
         String uri = "test";
 
-        OfferDto offerDto = new OfferDto("title","example","test","test");
-        OfferDto offerDto2 = new OfferDto("title2","example2","test","test");
-
-        when(restTemplate.exchange(
-                ArgumentMatchers.anyString(),
-                ArgumentMatchers.any(HttpMethod.class),
-                ArgumentMatchers.any(),
-                ArgumentMatchers.<ParameterizedTypeReference<List<OfferDto>>>any()
-        )).thenReturn(new ResponseEntity<>(Arrays.asList(offerDto, offerDto2), HttpStatus.ACCEPTED));
+        when(getExchange(restTemplate))
+                .thenReturn(responseWithTwoOffers());
 
         OfferHttpClient offerHttpClient = new OfferHttpClient(restTemplate, uri);
 
