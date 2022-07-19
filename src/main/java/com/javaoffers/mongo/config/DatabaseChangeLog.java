4b20fad61662d6b7d90ae2f89a9d4cfe722d7894
lauur <92169813+lauur@users.noreply.github.com>
2022-07-19 22:41:00 +0200

    implemented mongock

---
+++ src/main/java/com/javaoffers/mongo/config/DatabaseChangeLog.java
@@ -0,0 +1,30 @@
+package com.javaoffers.mongo.config;
+
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import com.javaoffers.offer.domain.Offer;
+import com.javaoffers.offer.domain.OfferRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ChangeLog
+public class DatabaseChangeLog {
+
+    @ChangeSet(author = "lauur", id = "seedDatabase", order = "001")
+    public void seedDatabase(OfferRepository offerRepository) {
+        List<Offer> offerList = new ArrayList<>();
+        offerList.add(offer1());
+        offerList.add(offer2());
+
+        offerRepository.insert(offerList);
+    }
+
+    private Offer offer2() {
+        return new Offer("2", "Junior Android Developer", "XYZ", "7k - 10k PLN", "https://example.com");
+    }
+
+    private Offer offer1() {
+        return new Offer("1", "Junior Java Developer", "ABC", "6k - 8k PLN", "https://example.com");
+    }
+}
