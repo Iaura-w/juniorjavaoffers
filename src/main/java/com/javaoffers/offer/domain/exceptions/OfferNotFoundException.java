3bbd1d048b10057549129ef41765b5617fda218d
lauur <92169813+lauur@users.noreply.github.com>
2022-07-27 19:18:19 +0200

    edited exception

--- src/main/java/com/javaoffers/offer/domain/exceptions/OfferNotFoundException.java
+++ src/main/java/com/javaoffers/offer/domain/exceptions/OfferNotFoundException.java
@@ -2,9 +2,13 @@ package com.javaoffers.offer.domain.exceptions;
 
 public class OfferNotFoundException extends RuntimeException {
 
-    public static final String MESSAGE = "Offer with id %s was not found";
+    private static final String MESSAGE = "Offer with id %s was not found";
 
     public OfferNotFoundException(String offerId) {
-        super(String.format(MESSAGE, offerId));
+        super(getMessage(offerId));
+    }
+
+    private static String getMessage(String offerId) {
+        return String.format(MESSAGE, offerId);
     }
 }
