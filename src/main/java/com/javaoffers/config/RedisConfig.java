5c70419cf7278edb07e858fa8b44b5db1061ec7d
lauur <92169813+lauur@users.noreply.github.com>
2022-08-03 21:53:58 +0200

    fixed tests

--- src/main/java/com/javaoffers/config/RedisConfig.java
+++ src/main/java/com/javaoffers/config/RedisConfig.java
@@ -1,12 +1,16 @@
 package com.javaoffers.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @Configuration
+@ConditionalOnProperty("redis.cache.enabled")
+@EnableCaching
 public class RedisConfig {
 
     @Bean
