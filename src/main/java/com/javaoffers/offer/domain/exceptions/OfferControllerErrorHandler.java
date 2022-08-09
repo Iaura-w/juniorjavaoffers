274054bd710a326dc9b6424141baef199cb6bd70
lauur <92169813+lauur@users.noreply.github.com>
2022-08-09 10:44:35 +0200

    added validation error handler

--- src/main/java/com/javaoffers/offer/domain/exceptions/OfferControllerErrorHandler.java
+++ src/main/java/com/javaoffers/offer/domain/exceptions/OfferControllerErrorHandler.java
@@ -2,16 +2,18 @@ package com.javaoffers.offer.domain.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
-public class OfferControllerErrorHandler extends ResponseEntityExceptionHandler {
+public class OfferControllerErrorHandler {
 
     @ExceptionHandler(OfferNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -19,5 +21,17 @@ public class OfferControllerErrorHandler extends ResponseEntityExceptionHandler 
         String message = e.getMessage();
         log.error(message);
         return new OfferErrorResponse(ZonedDateTime.now(), HttpStatus.NOT_FOUND, message);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public OfferErrorResponse handleValidationException(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getField() + " - " + x.getDefaultMessage())
+                .collect(Collectors.toList());
+        log.error(errors.toString());
+        return new OfferErrorResponse(ZonedDateTime.now(), HttpStatus.BAD_REQUEST, errors.toString());
     }
 }
