0daf8d558cec2e25e103296e5f31beef8a80d146
lauur <92169813+lauur@users.noreply.github.com>
2022-08-09 14:48:53 +0200

    edited post mapping

--- src/main/java/com/javaoffers/offer/OfferController.java
+++ src/main/java/com/javaoffers/offer/OfferController.java
@@ -4,15 +4,19 @@ import com.javaoffers.offer.domain.OfferService;
 import com.javaoffers.offer.domain.dto.OfferDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,14 +34,22 @@ public class OfferController {
 
     @GetMapping("/{id}")
     public ResponseEntity<OfferDto> getOfferById(@PathVariable String id) {
-        log.info("Request for offer with id " + id);
+        log.info("Request for offer with id {}", id);
         return ResponseEntity.ok(service.getOfferById(id));
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<OfferDto> addOffer(@Valid @RequestBody OfferDto offerDto) {
         OfferDto savedOffer = service.saveOffer(offerDto);
         log.info("Added new offer");
-        return ResponseEntity.ok(savedOffer);
+        return ResponseEntity.created(getSavedOfferUri(savedOffer)).body(savedOffer);
+    }
+
+    private static URI getSavedOfferUri(OfferDto savedOffer) {
+        return ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/" + savedOffer.getId())
+                .build()
+                .toUri();
     }
 }
