bb8f4103f9326b7ae9ccb7252a39fa7e57e66783
lauur <92169813+lauur@users.noreply.github.com>
2022-09-02 18:49:10 +0200

    refactored

--- src/main/java/com/javaoffers/security/login/LoginController.java
+++ src/main/java/com/javaoffers/security/login/LoginController.java
@@ -1,16 +1,11 @@
 package com.javaoffers.security.login;
 
-import com.javaoffers.security.jwt.JwtUtils;
 import com.javaoffers.security.login.domain.AppUser;
 import com.javaoffers.security.login.domain.UserService;
 import com.javaoffers.security.login.domain.dto.LoginRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,24 +16,13 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
-
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtils jwtUtils;
     private final UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@Valid @RequestBody LoginRequestDto loginRequestDto) {
-        UsernamePasswordAuthenticationToken authenticationToken = getAuthenticationToken(loginRequestDto);
-        Authentication authentication = authenticationManager.authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String token = jwtUtils.generateJwtToken(authentication);
+        String token = userService.generateTokenForUser(loginRequestDto);
         log.info("User '{}' logged in", loginRequestDto.getUsername());
         return ResponseEntity.ok().header("Authorization", "Bearer " + token).build();
-    }
-
-    private static UsernamePasswordAuthenticationToken getAuthenticationToken(LoginRequestDto loginRequestDto) {
-        return new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword());
     }
 
     @PostMapping("/register")
--- src/main/java/com/javaoffers/security/login/domain/UserService.java
+++ src/main/java/com/javaoffers/security/login/domain/UserService.java
@@ -1,8 +1,14 @@
 package com.javaoffers.security.login.domain;
 
-import com.javaoffers.security.login.domain.exception.DuplicateUsernameException;
+import com.javaoffers.security.jwt.JwtUtils;
+import com.javaoffers.security.login.domain.dto.LoginRequestDto;
+import com.javaoffers.security.login.domain.exceptions.DuplicateUsernameException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +18,22 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    public String generateTokenForUser(LoginRequestDto loginRequestDto) {
+        UsernamePasswordAuthenticationToken authenticationToken = getAuthenticationToken(loginRequestDto);
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return jwtUtils.generateJwtToken(authentication);
+    }
+
+    private static UsernamePasswordAuthenticationToken getAuthenticationToken(LoginRequestDto loginRequestDto) {
+        return new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword());
+    }
 
     public AppUser registerUser(String username, String password) {
-
         try {
             AppUser user = new AppUser(username, passwordEncoder.encode(password));
             return userRepository.save(user);
